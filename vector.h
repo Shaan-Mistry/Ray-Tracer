827f0e8297fe35e65ccbfe5f4f7b391553911f37
Shaan-Mistry <shaanmistry03@gmail.com>
2022-07-04 17:56:30 -0700

    Added Arithmetic Operators

--- vector.h
+++ vector.h
@@ -15,24 +15,32 @@ private:
     double e[3];
 public:
     // Class Constructors & Destructors ----------------------------------------
-vec3();
+    vec3();
     vec3(double x, double y, double z);
     ~vec3();
 
     // Access functions --------------------------------------------------------
+    double x() const;
+    double y() const;
+    double z() const;
     double magnitude() const;
-    double dot_product(const vec3 &V) const;
+    double dot_product(const vec3 &v) const;
 
     // Overriden Operators -----------------------------------------------------
-    bool operator==(const vec3 &V);
-    vec3& operator+=(const vec3 &V);
+    bool operator==(const vec3 &v);
+    vec3& operator+=(const vec3 &v);
     vec3& operator*=(const double a);
     vec3& operator/=(const double a);
     double operator[](int i) const;
     double& operator[](int i);
-
-    friend std::ostream& operator<<(std::ostream &stream, const vec3 &V);
-
 };
+    inline std::ostream& operator<<(std::ostream &stream, const vec3 &v);
+    inline vec3 operator+(const vec3 &u, const vec3 &v);
+    inline vec3 operator-(const vec3 &u, const vec3 &v);
+    inline vec3 operator*(const vec3 &u, const vec3 &v);
+    inline vec3 operator*(double a, const vec3 &v);
+    inline vec3 operator*(const vec3 &u, double a);
+    inline vec3 operator/(const vec3 &u, double a);
 
+using color = vec3;
 #endif
