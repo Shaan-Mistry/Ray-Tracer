9d444e6143e0eea15e0e58273d7b80cbf06b0afd
Shaan-Mistry <shaanmistry03@gmail.com>
2022-07-21 15:59:33 -0700

    save

--- sphere.h
+++ sphere.h
@@ -18,17 +18,17 @@
 //  dist = (-b - sqrt(discriminant)) / 2a
 
 #include "ray.h"
+#include "object.h"
 #include <cmath>
 
 #ifndef SPHERE_H_INCLUDE_
 #define SPHERE_H_INCLUDE_
 
-class sphere {
+class sphere : public object {
 
 private:
     point center;
     double radius;
-
 
 public:
 std::string material;
@@ -40,19 +40,34 @@ std::string material;
     // Checks if the inputted ray intersects the sphere.
     // If there is an intersection, it returns the distance point from the
     // origin ray. If there is no intersection, function returns -1.
-    double intersects(ray r) {
+    virtual bool hit(ray &r, double t_min, double t_max, hit_record &record) const {
         vector sphere_to_ray = r.origin() - center;
-
         auto a = r.direction().dot_product(r.direction());
         auto b = 2 * r.direction().dot_product(sphere_to_ray);
         auto c = sphere_to_ray.dot_product(sphere_to_ray) - radius * radius;
         auto discriminant = b * b - 4 * a * c;
 
         if (discriminant < 0) {
-            return -1.0;
-        } else {
-            return (-b - sqrt(discriminant)) / (2 * a);
+            return false;
         }
+        auto sqrtd = sqrt(discriminant);
+
+        // Find nearest root between t_min and t_max.
+        auto root = (-b - sqrt(discriminant)) / (2 * a);
+        if (root < t_min || t_max < root) {
+            //root = (-half_b + sqrtd) / a;
+            if (root < t_min || t_max < root) {
+                return false;
+            }
+        }
+        // Update hit record values.
+        record.t = root;
+        record.p = r.location(rec.t);
+        record.normal = (record.p - center) / radius;
+        vector outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+        return true;
+
     }
 };
 #endif
